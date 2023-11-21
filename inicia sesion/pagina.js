e45452a5e18bebd8757ede35fd35754dcba43dd6
dannagarcia211 <140906549+dannagarcia211@users.noreply.github.com>
2023-11-21 15:33:18 -0500

    initial commit

---
+++ inicia sesion/pagina.js
@@ -0,0 +1,19 @@
+const $submit = document.getElementById("submit"),
+      $password = document.getElementById("password"),
+      $username = document.getElementById("username"),
+      $visible = document.getElementById("visible");
+document.addEventListener("change"), (e)=>{
+    if(e.target === $visible){
+        if($visible.checked === false) $password.type = "password"
+        else $password.type = "text";
+    }
+};
+
+document.addEventListener("click"), (e)=>{
+    if(e.target === $visible){
+        if($password.value !== "" && $username.value !== ""){
+            e.preventDefault();
+            window.location.href = "home.html"
+        }
+    }
+}
